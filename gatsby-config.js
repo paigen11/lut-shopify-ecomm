dd31a2db7056257741315673b4fd4caa16003268
Paige Niedringhaus <paigen11@gmail.com>
2020-03-19 11:48:10 -0400

    first commit with shopify;

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `LUT Shopify Store`,
+    description: `Find all the stuff you need`,
+    author: `@pniedri`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
