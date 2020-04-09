06da1131bcb5b663b1bcedd1b14bcf57e38439a9
Paige Niedringhaus <paigen11@gmail.com>
2020-04-09 11:31:19 -0400

    adding product links

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "gatsby";
 import Img from "gatsby-image";
 
 export const Product = ({ product }) => {
   return (
-    <article>
-      <Img fixed={product.images[0].localFile.childImageSharp.fixed} />
-      <h3>{product.title}</h3>
-    </article>
+    <Link to={`/products/${product.handle}`}>
+      <article>
+        <Img fixed={product.images[0].localFile.childImageSharp.fixed} />
+        <h3>{product.title}</h3>
+      </article>
+    </Link>
   );
 };
